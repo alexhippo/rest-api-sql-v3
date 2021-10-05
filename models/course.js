a5b28dbd2eb79be923131e70596cf46909773d5c
Alex Hipolito <alexheepo@gmail.com>
2021-10-05 23:39:53 +1100

    Validate course title and course description

--- models/course.js
+++ models/course.js
@@ -9,10 +9,28 @@ module.exports = (sequelize) => {
       autoIncrement: true
     },
     title: {
-      type: DataTypes.STRING
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'A title for the course is required.'
+        },
+        notEmpty: {
+          msg: 'Please provide a title for the course.'
+        }
+      }
     },
     description: {
-      type: DataTypes.TEXT
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'A course description is required.'
+        },
+        notEmpty: {
+          msg: 'Please provide a course description.'
+        }
+      }
     },
     estimatedTime: {
       type: DataTypes.STRING
