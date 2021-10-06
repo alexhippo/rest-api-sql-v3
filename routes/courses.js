c30c6a3128cf19414b6cd30c3b1990a3f3d614de
Alex Hipolito <alexheepo@gmail.com>
2021-10-06 21:55:45 +1100

    Only allow authenticated user to update/delete courses they are owners of

--- routes/courses.js
+++ routes/courses.js
@@ -61,12 +61,17 @@ router.post('/courses', authenticateUser, asyncHandler(async (req, res) => {
 
 // Update an existing course
 router.put("/courses/:id", authenticateUser, asyncHandler(async (req, res, next) => {
+  const user = req.currentUser;
   let course;
   try {
     course = await Course.findByPk(req.params.id);
     if (course) {
-      await course.update(req.body);
-      res.status(204).end();
+      if (course.userId === user.id) {
+        await course.update(req.body);
+        res.status(204).end();
+      } else {
+        res.status(403).json({ error: 'You are not authorised to update this course.' });
+      }
     } else {
       const err = new Error(`Course Not Found`);
       res.status(404).json({ error: err.message });
@@ -83,10 +88,15 @@ router.put("/courses/:id", authenticateUser, asyncHandler(async (req, res, next)
 
 // Delete an existing course
 router.delete("/courses/:id", authenticateUser, asyncHandler(async (req, res, next) => {
+  const user = req.currentUser;
   const course = await Course.findByPk(req.params.id);
   if (course) {
-    await course.destroy();
-    res.status(204).end();
+    if (course.userId === user.id) {
+      await course.destroy();
+      res.status(204).end();
+    } else {
+      res.status(403).json({ error: 'You are not authorised to delete this course.' });
+    }
   } else {
     const err = new Error(`Course Not Found`);
     res.status(404).json({ error: err.message });
